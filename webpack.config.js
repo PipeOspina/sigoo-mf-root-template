6522105f3e7735a89d1f56053bce07d64ff94364
Pipe Ospina <pipe.98.oh@gmail.com>
2023-10-02 23:27:59 -0500

    add gcloud app deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || `localhost:${port}`;
+
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "cm-sigoo";
   const defaultConfig = singleSpaDefaults({
@@ -14,6 +17,9 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port,
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
@@ -22,6 +28,8 @@ module.exports = (webpackConfigEnv, argv) => {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
+        port,
+        host,
       }),
     ],
   });
